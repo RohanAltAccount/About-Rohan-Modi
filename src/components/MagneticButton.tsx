36a08a22a71d68821c555103457a12df40a8d858
Rohan Modi <rohanmodi1014@gmail.com>
2026-05-17 05:57:16 +0000

    feat: subscribe button

---
+++ src/components/MagneticButton.tsx
@@ -0,0 +1,51 @@
+import { useRef, useState } from "react";
+
+export default function MagneticButton() {
+  const ref = useRef<HTMLButtonElement>(null);
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  function handleMouseMove(e: React.MouseEvent<HTMLButtonElement>) {
+    const button = ref.current;
+    if (!button) return;
+
+    const rect = button.getBoundingClientRect();
+
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+
+    setPos({
+      x: x * 0.25,
+      y: y * 0.25,
+    });
+  }
+
+  function handleMouseLeave() {
+    setPos({ x: 0, y: 0 });
+  }
+
+  return (
+    <button
+      ref={ref}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        transform: `translate(${pos.x}px, ${pos.y}px)`,
+      }}
+      className="
+        min-w-[250px]
+        min-h-[30px]
+        border-black/500
+        border-1
+        cursor-pointer rounded-2xl
+        bg-gradient-to-b from-white-400 to-white-700
+        px-10 py-4 font-medium text-gray-750
+        shadow-lg shadow-yellow-500/30
+        ring-1 ring-white/20
+        transition-transform duration-150 ease-out
+        active:scale-95
+      "
+    >
+      Subscribe @ROHAN-MODI
+    </button>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <a href = "https://www.youtube.com/@ROHAN-MODI"
+            className="link">YouTube</a>
+            <a href="https://thetechresearchpaper.wixsite.com/techknowledge/blog"
+            className="link">Blog</a>
+            <a href="https://www.github.com/RohanAltAccount"
+            className="link">Github</a>
+        </nav>
+    );
+}
+
