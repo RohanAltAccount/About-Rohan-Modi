4526ad8b280de95819555d07f65860b33e2fb61e
Rohan Modi <rohanmodi1014@gmail.com>
2026-05-19 17:19:15 -0500

    Add 'Writer' to words in ContainerTextFlipDemo

--- src/components/ContainerTextFlip.tsx
+++ src/components/ContainerTextFlip.tsx
@@ -3,7 +3,7 @@ import { ContainerTextFlip } from "@/components/ui/container-text-flip";
 export function ContainerTextFlipDemo() {
   return (
     <ContainerTextFlip
-      words={["Rohan Modi", "Student", "Tech Nerd", "Creator", "Developer", "Designer"]}
+      words={["Rohan Modi", "Student", "Tech Nerd", "Creator", "Developer", "Designer", "Writer"]}
     />
   );
 }
